b6008ae7c778c8783935af01840d425938a4a130
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-07-19 19:17:14 -0700

    Make calculator html and css
    Made the look for the calculator. No js done yet.

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+let firstNumber = 0
+let operator = 0
+let secondNumber = 0
+
+function operate(firstNumber, operator, secondNumber) {
+
+}
+
+const display = document.querySelector('#display')
+
+const button = document.querySelectorAll('.box').forEach(button => 
+    button.addEventListener('click', () => {
+        firstNumber = button.id
+    }
+))
