eca723b57c025334deb499feaa90bb59333a28bf
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-07-20 18:23:32 -0700

    Make operate function
    Made the opertate function to do all the math when the equals sign is clicked.

--- script.js
+++ script.js
@@ -14,27 +14,53 @@ function divide(a, b) {
     return a / b
 }
 
+function operate(displayString) {
+    splitString = displayString.split('')
+    operator = splitString.filter(operator => isNaN(operator)).join()
+    firstNumber = splitString.slice(0, splitString.indexOf(operator)).join('')
+    secondNumber = splitString.slice(splitString.indexOf(operator) + 1).join('')
+
+    if (operator === '+') {
+        return add(Number(firstNumber), Number(secondNumber))
+    }
+    else if (operator === '-') {
+        return subtract(Number(firstNumber), Number(secondNumber))
+    }
+    else if (operator === '*') {
+        return multiply(Number(firstNumber), Number(secondNumber))
+    }
+    else if (operator === '/') {
+        return divide(Number(firstNumber), Number(secondNumber))
+    }
+}
+
+function clear() {
+    displayString = ''
+}
+
 let firstNumber = ''
 let operator = ''
 let secondNumber = ''
-
-function operate(firstNumber, operator, secondNumber) {
-
-}
+let displayString = ''
 
 const display = document.querySelector('#display')
 
 const button = document.querySelectorAll('.number').forEach(button => 
     button.addEventListener('click', () => {
-        firstNumber += button.id
-        display.textContent = firstNumber
-        console.log(firstNumber)
+        displayString += button.id
+        display.textContent = displayString
     }
 ))
 
 const operatorButton = document.querySelectorAll('.operator').forEach(operatorButton => 
     operatorButton.addEventListener('click', () => {
-        operator = operatorButton.id
-        display.textContent = operator
-    })
+        displayString += operatorButton.id
+        display.textContent = displayString
+    }, {once : true})
+)
+
+const equals = document.querySelector('.equals')
+equals.addEventListener('click', () => {
+        display.textContent = operate(displayString)
+    }
 )
